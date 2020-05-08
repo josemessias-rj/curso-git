529d0cbb2c561c31c52930491f2a3f9d67a39afb
Jose Messias <zemessiasrj@gmail.com>
2020-05-07 21:17:13 -0300

    Teste de commit inicial

---
+++ Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso.NetCore
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public double IncreaseSalary(double salary, double percentual)
+        {
+            Salary += (salary * percentual) / 100; 
+            return Salary;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Curso.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a quantidade de funcionarios: ");
+            int n = int.Parse(Console.ReadLine());            
+
+            List<Funcionario> funcionarioList = new List<Funcionario>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Funcionario funcionario = new Funcionario();
+
+                Console.Write("Informe o Id do funcionário: ");
+                funcionario.Id = int.Parse(Console.ReadLine());
+
+                Console.Write("Informe o nome do funcionário: ");
+                funcionario.Name = Console.ReadLine();
+
+                Console.Write("Informe o salário do funcionário: ");
+                funcionario.Salary = double.Parse(Console.ReadLine());
+
+                funcionarioList.Add(funcionario);
+            }
+
+            Console.Write("Informe o código do funcionário a receber promoção salarial: ");
+            int id = int.Parse(Console.ReadLine());
+
+            if (funcionarioList.Find(x => x.Id == id) != null)
+            {
+                Console.Write("Informe o percentual de aumento: ");
+                double perc = double.Parse(Console.ReadLine());
+
+                Funcionario func = funcionarioList.Find(x => x.Id == id);
+                func.IncreaseSalary(func.Salary, perc);
+            }
+            else
+            {
+                Console.WriteLine("Id inexistente.");
+            }
+
+
+            Console.WriteLine("Lista de Funcionário");
+            foreach (Funcionario list in funcionarioList)
+            {
+                Console.WriteLine($"{list.Id}, {list.Name}, {list.Salary}");
+            }
+        }
+    }
+}
